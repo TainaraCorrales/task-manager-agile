647002a2e29f51c82389ca73fc77c597d528cee2
Tainara Corrales <tainaracorrales@gmail.com>
2026-01-07 14:43:13 -0300

    Update test_tasks.py
    fix: simplificar testes para corrigir erro de sintaxe

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,22 +1,16 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app
 
 def test_create_task():
     client = app.test_client()
-    response = client.post(
-        '/tasks',
-        json={
-            'title': 'Nova tarefa',
-            'priority': 'Alta'
-        }
-    )
+    response = client.post("/tasks", json=dict(title="Nova tarefa"))
     assert response.status_code == 201
 
 def test_get_tasks():
     client = app.test_client()
-    response = client.get('/tasks')
+    response = client.get("/tasks")
     assert response.status_code == 200
